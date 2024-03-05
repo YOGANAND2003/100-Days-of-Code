9df2f0adfa086195a0b2f86a27eb69c393b5e20d
Jammisetti Yoganand <91559291+YOGANAND2003@users.noreply.github.com>
2024-03-05 23:28:31 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/First and last occurrences of X/first-and-last-occurrences-of-x.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> firstAndLast(vector<int> &arr, int n, int x) {
+        // Code here
+        vector<int> ans;
+        for(int i=0; i<n; i++){
+            if(arr[i]==x){
+                ans.push_back(i);
+            }
+        }
+        int a=ans.size();
+        if(a==0){
+            ans.push_back(-1);
+            
+            return ans;
+        }
+        else{
+            return {ans[0],ans[a-1]};
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        Solution obj;
+        vector<int> ans= obj.firstAndLast(arr, n, x) ;
+        for(int i:ans){
+            cout<<i<<" ";
+        }
+        cout<< endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
